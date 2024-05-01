027df646067b02c7b012f42ff3b67f2c9e21b58e
Vitor Fauste <vitorhugo8127@gmail.com>
2024-05-01 11:15:19 -0300

    Criação do início da estrutura básica do sistema

--- BusinessLogicalLayer/Estacionamento.cs
+++ BusinessLogicalLayer/Estacionamento.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BusinessLogicalLayer
 {
-    internal class Estacionamento
+    public class Estacionamento
     {
     }
 }
---
+++ Shared/Response.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Response
+    {
+        public Response(string message, bool hasSuccess)
+        {
+            Message = message;
+            HasSuccess = hasSuccess;
+        }
+
+        public string Message { get; set; }
+        public bool HasSuccess { get; set; }
+    }
+}
